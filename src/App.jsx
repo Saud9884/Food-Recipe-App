90b19d3967f528820c19a89b829527da40689697
Saud9884 <saudlohar85@gmail.com>
2024-01-18 16:08:29 +0530

    Completed Navbar component

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Navbar from "./components/navbar";
+import Home from "./pages/home";
+import Favorites from "./pages/favorites";
+import Details from "./pages/details";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <div>
+        <div className="min-h-screen p-2 bg-white text-gray-600 text-lg">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favorites" element={<Favorites />} />
+            <Route path="/recipe-item/:id" element={<Details />} />
+          </Routes>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
